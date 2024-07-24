1e1e3b6bb093da7ba2047fc5438eb79e1d7e178d
Supriya Pandurangacharya Upadhyaya <supriya.upadhyaya@st.ovgu.de>
2024-07-24 14:30:02 +0200

    updates

--- attack/attackDataset.py
+++ attack/attackDataset.py
@@ -48,8 +48,8 @@ class AttackDataset:
         for path in jsonPaths:
             test_human_dataset, test_machine_dataset = self.load_and_process_jsonl_dataset(jsonPaths[path], col_names[path]["human_text_column"], col_names[path]["machine_text_column"], 0, 1)
             test_dataset = concatenate_datasets([test_human_dataset, test_machine_dataset])
-            X_test = test_dataset["text"].tolist()
-            y_test = test_dataset["label"].tolist()
+            X_test = list(test_dataset["text"])
+            y_test = list(test_dataset["label"])
             textattack_dataset = TextAttackDataset(list(zip(X_test, y_test)))
             #textattack_dataset = TextAttackDataset(list(test_dataset))
             print("Here is the dataset!")
