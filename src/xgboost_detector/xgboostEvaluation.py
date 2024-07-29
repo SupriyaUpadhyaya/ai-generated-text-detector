71e7bdde9a75bbfff060a758af78e52ac4ba2697
Supriya Pandurangacharya Upadhyaya <supriya.upadhyaya@st.ovgu.de>
2024-07-29 18:28:25 +0200

    update

--- src/xgboost_detector/xgboostEvaluation.py
+++ src/xgboost_detector/xgboostEvaluation.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import shap
 from src.xgboost_detector.featureExtractor import FeatureExtractor
 from src.shared import results_report
+import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
