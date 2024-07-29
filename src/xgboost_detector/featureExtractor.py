022de79d69132bbb610431bef8073c282ee04704
Supriya Pandurangacharya Upadhyaya <supriya.upadhyaya@st.ovgu.de>
2024-07-29 14:42:28 +0200

    update

--- src/xgboost_detector/featureExtractor.py
+++ src/xgboost_detector/featureExtractor.py
@@ -389,9 +389,10 @@ class FeatureExtractor():
         #df['sent_len_diff_human'] = self.normalize_column(df['sent_len_diff_human'])
         df['std_dev_machine'] = self.normalize_column(df['std_dev_machine'])
         df['sent_len_diff_machine'] = self.normalize_column(df['sent_len_diff_machine'])
-        print(df.values.tolist()[0])
-        return df.values.tolist()[0]
+        print(df.values.tolist())
+        return df.values.tolist()
     
     def getFeatures(self, text_input_list):
         input_features = np.array([self.featureExtractor(text) for text in text_input_list])
-        return input_features
+        df = pd.DataFrame(input_features)
+        return df
