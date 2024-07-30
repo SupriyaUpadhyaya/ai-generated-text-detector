5679fd7c83a76b94670fbe641317d9b4436684e4
Shashankhmg <shashankh.m.g.1999@gmail.com>
2024-07-30 13:01:52 +0200

    change 1

--- src/attack/attack.py
+++ src/attack/attack.py
@@ -54,7 +54,7 @@ class Attack:
                 print(f"No weights found at {weights_path}. Using the pre-trained model without additional weights.")
             self.model_wrapper = HuggingFaceModelWrapper(self.model, self.tokenizer)
         self.metrics = Metrics(self.log_path)
-        self.attack_recipe = ['textfoolerjin2019']
+        self.attack_recipe = ['pwws', 'pruthi', 'deep-word-bug']
         #self.attack_recipe = ['pwws', 'textfoolerjin2019', 'pruthi']
         print(f'************************** LOG PATH - {self.log_path} ***********************')
     
